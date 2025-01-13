41ea6626daf2339c209e1226fb10d27424099e96
Nguyen Quy Than <quythan306@gmail.com>
2025-01-13 23:51:01 +0700

    refreshToken

--- src/main/java/com/shoppproduct/dream_shops/auth/controller/AuthController.java
+++ src/main/java/com/shoppproduct/dream_shops/auth/controller/AuthController.java
@@ -1,15 +1,23 @@
 package com.shoppproduct.dream_shops.auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shoppproduct.dream_shops.auth.model.RefreshToken;
+import com.shoppproduct.dream_shops.auth.model.User;
 import com.shoppproduct.dream_shops.auth.service.AuthService;
+import com.shoppproduct.dream_shops.auth.service.JwtService;
+import com.shoppproduct.dream_shops.auth.service.RefreshTokenService;
 import com.shoppproduct.dream_shops.auth.utils.request.LoginRequest;
+import com.shoppproduct.dream_shops.auth.utils.request.RefreshTokenRequest;
 import com.shoppproduct.dream_shops.auth.utils.request.RegisterRequest;
+import com.shoppproduct.dream_shops.auth.utils.respone.AuthRespone;
 import com.shoppproduct.dream_shops.utils.response.ApiResponse;
 
 @RestController
@@ -19,6 +27,12 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
     @PostMapping("/register")
     public ResponseEntity<ApiResponse> register(@RequestBody RegisterRequest registerRequest){
         return ResponseEntity.ok(new ApiResponse("Register Successfully!", authService.register(registerRequest)));
@@ -26,7 +40,28 @@ public class AuthController {
     
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest loginRequest){
-        return ResponseEntity.ok(new ApiResponse("Login Successfully!", authService.login(loginRequest)));
+        try {
+            return ResponseEntity.ok(new ApiResponse("Login Successfully!", authService.login(loginRequest)));
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
     }
+
+    @PostMapping("/refreshToken")
+    public ResponseEntity<ApiResponse> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest){
+
+        RefreshToken refreshToken = refreshTokenService.verifyRefreshToken(refreshTokenRequest.getRefreshToken());
+
+        User user = refreshToken.getUser();
+
+        String accessToken = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(new ApiResponse("Refresh Token Successfully", AuthRespone.builder()
+                                                                    .accessToken(accessToken)
+                                                                    .refreshToken(refreshToken.getRefreshToken())
+                                                                    .build()));
+
+    }
+
 
 }
---
+++ src/main/java/com/shoppproduct/dream_shops/auth/utils/request/RefreshTokenRequest.java
@@ -0,0 +1,10 @@
+package com.shoppproduct.dream_shops.auth.utils.request;
+
+import lombok.Data;
+
+@Data
+public class RefreshTokenRequest {
+    
+   private String refreshToken;
+
+}
